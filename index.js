b3559ece27ea02835743862c9355d6775382d592
Onur Can Sevinc <onurcan.svnc@yandex.com>
2025-04-09 20:52:44 +0300

    feat: serve index.html in HTTP server response

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+const fs = require('fs');
+const path = require('path');
 const http = require('http');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    res.end('Hello World');
+    let filePath;
+
+    switch (req.url) {
+        case '/':
+            filePath = path.join(__dirname, 'pages', 'index.html');
+            break;
+        default:
+            // 404 not found
+            res.writeHead(404);
+            res.end('404 Not Found');
+            return;
+    }
+
+    try {
+        const content = fs.readFileSync(filePath, 'utf8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+    } catch (error) {
+        res.writeHead(500);
+        res.end('Internal Server Error');
+    }
 });
 
 server.listen(PORT, () => {
