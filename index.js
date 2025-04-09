e97c09796329662bc379df9fa712905d883507a4
Onur Can Sevinc <onurcan.svnc@yandex.com>
2025-04-09 21:09:05 +0300

    feat: implement HTML routes and add employeeList API endpoint in HTTP server

--- index.js
+++ index.js
@@ -7,33 +7,51 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
     let filePath;
 
-    switch (req.url) {
-        case '/':
-            filePath = path.join(__dirname, 'pages', 'index.html');
-            break;
-        case '/index':
-            filePath = path.join(__dirname, 'pages', 'index.html');
-            break;
-        case '/products':
-            filePath = path.join(__dirname, 'pages', 'products.html');
-            break;
-        case '/connect':
-            filePath = path.join(__dirname, 'pages', 'connect.html');
-            break;
-        default:
-            // 404 not found
-            res.writeHead(404);
-            res.end('404 Not Found');
-            return;
+    // HTML routes
+    if (req.url === '/' || req.url === '/products' || req.url === '/connect') {
+        switch (req.url) {
+            case '/':
+                filePath = path.join(__dirname, 'pages', 'index.html');
+                break;
+            case '/index':
+                filePath = path.join(__dirname, 'pages', 'index.html');
+                break;
+            case '/products':
+                filePath = path.join(__dirname, 'pages', 'products.html');
+                break;
+            case '/connect':
+                filePath = path.join(__dirname, 'pages', 'connect.html');
+                break;
+        }
+
+        try {
+            const content = fs.readFileSync(filePath, 'utf8');
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        } catch (error) {
+            res.writeHead(500);
+            res.end('Internal Server Error');
+        }
     }
 
-    try {
-        const content = fs.readFileSync(filePath, 'utf8');
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
-    } catch (error) {
-        res.writeHead(500);
-        res.end('Internal Server Error');
+    // API Endpoints
+    switch (req.url) {
+        case '/employeeList':
+            try {
+                const filePath = path.join(__dirname, 'data', 'employeeList.json');
+                const employees = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+                const employeesWithoutSalary = employees.map(({ maas, ...employee }) => employee); // get employees without the 'maas'
+
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(employeesWithoutSalary));
+            } catch (error) {
+                res.writeHead(500);
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            }
+            break;
+        default:
+            res.writeHead(404);
+            res.end(JSON.stringify({ error: '404 Not Found' }));
     }
 });
 
